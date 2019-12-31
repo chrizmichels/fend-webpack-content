eeec2663d3c38f3ba12e79dd762d5e2858f2ce6a
chrizmichels <christian@4arge.com>
2020-01-01 00:22:00 +0100

    updates

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
 import { handleSubmit } from "./js/formHandler";
 import { checkForName } from "./js/nameChecker";
+import img from "../client/views/file.jpg";
 
 console.log(checkForName);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -9,7 +10,21 @@ module.exports = {
         test: "/.js$/",
         exclude: /node_modules/,
         loader: "babel-loader"
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
